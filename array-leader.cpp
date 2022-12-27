e50ef33b89898de715403326f744af8cbbc96aca
osdev07 <108567152+osdev07@users.noreply.github.com>
2022-12-27 08:17:08 +0530

    Create array-leader.cpp

---
+++ array-leader.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        // first element is always leader
+        int maxSoFar = a[n-1];
+        vector<int> result;
+        result.push_back(maxSoFar);
+        for (int i = n-2; i >= 0; i--) {
+            if (a[i] >maxSoFar ) {
+                maxSoFar = a[i];
+                result.push_back(maxSoFar);
+            }
+        }
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+
+// } Driver Code Ends
